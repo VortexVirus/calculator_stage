f008d943b123716e3363617f13e97793929cf069
Night <106701321+VortexVirus@users.noreply.github.com>
2025-12-17 14:54:06 +0100

    ui update
    l'ui est un petit peu faite aucune implémentation de calcul juste de l'ui

--- calculator de fou malade.py
+++ calculator de fou malade.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import ttk
-
+import tkinter as tk
 
 def calculatorPlus(chiffre, plus):
     chiffre_float = float(chiffre)
@@ -32,9 +32,24 @@ window = Tk() #créer le truc
 window.title("calculatrice") # donne le nom de la fenetre
 
 mainframe = ttk.Frame(window, padding=(30)) # créer une frame pour placer des choses dedans
-mainframe.grid(column=0, row=0)  # gére la geo
+mainframe.grid()  # gére la geo
 
-ttk.Button(window, text="calculate").grid(column=1, row=1) # créer un button avec marquer calculate dessus
+
+ttk.Button(window, text="1", ).grid(column=1, row=1)
+ttk.Button(window, text="2", ).grid(column=2, row=1)
+ttk.Button(window, text="3", ).grid(column=3, row=1)
+ttk.Button(window, text="4", ).grid(column=1, row=3)
+ttk.Button(window, text="5", ).grid(column=2, row=3)
+ttk.Button(window, text="6", ).grid(column=3, row=3)
+ttk.Button(window, text="7", ).grid(column=1, row=5)
+ttk.Button(window, text="8", ).grid(column=2, row=5)
+ttk.Button(window, text="9", ).grid(column=3, row=5)
+ttk.Button(window, text="0", ).grid(column=2, row=10)
+tk.Button(window, text="+").grid(column=4, row=2)
+tk.Button(window, text="=").grid(column=4, row=4)
+
+
+ttk.Button(window, text="calculate").grid(column=3, row=10) # créer un button avec marquer calculate dessus
 
 
 window.mainloop()  # print la fenetre
